55f5756507a0be5e277ac6a08b19df7463f6c057
TolgaKara <tolgakara2.0@gmail.com>
2021-04-02 10:39:59 +0200

    Added Navbar component

---
+++ src/components/organisms/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Branding } from "../../atoms/branding/Branding";
+import "./Navbar.css";
+
+export const Navbar = () => {
+	return (
+		<nav>
+			<Branding />
+		</nav>
+	);
+};
