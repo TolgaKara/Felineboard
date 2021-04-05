ef47f0bef96b6877116aae045a1ba9867bc38e60
TolgaKara <tolgakara2.0@gmail.com>
2021-04-05 08:10:57 +0200

    Added Hero to the landing page app

--- src/components/pages/app/App.js
+++ src/components/pages/app/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Branding } from "../../atoms/branding/Branding";
 import { Navbar } from "../../organisms/navbar/Navbar";
+import Hero from "../../molecules/hero/Hero";
 
 function App() {
 	return (
 		<div>
 			<Navbar />
+			<Hero />
 		</div>
 	);
 }
