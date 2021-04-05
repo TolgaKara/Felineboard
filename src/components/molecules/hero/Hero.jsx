848a96864f09ab726de712eecfd27a01cf6f4636
TolgaKara <tolgakara2.0@gmail.com>
2021-04-05 08:21:33 +0200

    Centered Div and added text

--- src/components/molecules/hero/Hero.jsx
+++ src/components/molecules/hero/Hero.jsx
@@ -2,5 +2,16 @@ import React from "react";
 import "./Hero.css";
 
 export default function Hero() {
-	return <div className='hero-img'>sdad</div>;
+	return (
+		<div className='hero-img'>
+			<div className='cta'>
+				<p className='cta-sub'>Want to be a good Owner then you are at the right place</p>
+				<h1 className='cta-title'>Tame your cat</h1>
+				<p className='cta-text'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+				<a href='' className='cta-btn'>
+					Get Started
+				</a>
+			</div>
+		</div>
+	);
 }
